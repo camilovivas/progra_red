482dbc8d94106b99643690b3ae96f546686758cb
camilovivas <47890754+camilovivas@users.noreply.github.com>
2020-09-28 10:11:17 -0500

    create window to chat

--- taller2/client/src/view/MultichatWindows.java
+++ taller2/client/src/view/MultichatWindows.java
@@ -6,14 +6,19 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class MultichatWindows extends Stage {
 
     private HBox hb;
 
     private ScrollPane scroll;
     private VBox clients;
+    private ArrayList<Button> clientsConnected;
+
     private VBox vb;
-    private VBox msg;
+
+    private TextArea messageArea;
 
     private HBox msgAndSend;
     private TextField message;
@@ -22,18 +27,42 @@ public class MultichatWindows extends Stage {
     private Scene scene;
     private MultiChatController controller;
 
+
     public MultichatWindows(){
         controller = new MultiChatController (this) ;
+        clientsConnected =  new ArrayList<> ( );
+        init ();
+    }
 
+    public void fillButtons(){
+        for (int i = 0; i < 50; i++) {
+            Button bt = new Button ( i+"" );
+            clients.getChildren ().add( bt);
+        }
     }
 
     public void init(){
+        hb = new HBox ( );
+
+        scroll = new ScrollPane ( );
+        clients = new VBox ( );
         scroll.setContent ( clients );
+        fillButtons ();
+
+        vb = new VBox ( );
+        messageArea = new TextArea ( );
+        messageArea.setEditable ( false );
 
         message = new TextField ( "Mensaje" );
         send = new Button ( ">" );
         msgAndSend = new HBox ( );
         msgAndSend.getChildren ().addAll ( message, send );
+        vb.getChildren ().addAll ( messageArea, msgAndSend );
+
+        hb.getChildren ().addAll ( scroll, vb );
+
+        scene = new Scene ( hb, 600,600 );
+        this.setScene ( scene );
 
     }
 
@@ -43,5 +72,9 @@ public class MultichatWindows extends Stage {
 
     public TextField getMessage ( ) {
         return message;
+    }
+
+    public TextArea getMessageArea ( ) {
+        return messageArea;
     }
 }
